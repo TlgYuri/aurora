a942e00124f04e02f6c49df48d789f73812bc034
tangluoguan ys44430 (CW, TS) <ys.tangluoguan@h3c.com>
2022-12-05 22:50:03 +0800

    1、sql增加数据库创建语句，新增低版本mysql使用的脚本  2、图标、页面标题、配置文件个人化，另外jdbcUrl新增字符集参数配置  3、页面默认语言改为中文

--- aurora-mysql8.sql
+++ aurora-mysql8.sql
@@ -1,6 +1,8 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE DATABASE IF NOT EXISTS `aurora` DEFAULT CHARACTER SET = utf8mb4;
+
 -- ----------------------------
 -- Table structure for t_about
 -- ----------------------------
